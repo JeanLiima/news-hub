2beb9503c41633371098aa728761616d7b4c7f3f
JeanLiima <jean.car.liima@gmail.com>
2025-07-25 19:52:58 -0300

    Finally fix build

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -3,19 +3,21 @@ import { type Metadata } from 'next';
 import { CategoryClientPage } from './components/client-page';
 
 type CategoryPageProps = {
-	readonly params: { category: string };
+	readonly params: Promise<{ category: string}>;
 };
 
 export async function generateMetadata(
 	{ params }: CategoryPageProps,
 ): Promise<Metadata> {
-	const categoryName = decodeURIComponent(params.category);
+	const { category } = await params || {}
+	const categoryName = decodeURIComponent(category);
 	return {
 		title: `Notícias sobre ${categoryName} | NewsHub`,
 		description: `Veja as últimas notícias sobre ${categoryName} no NewsHub.`,
 	};
 }
 
-export default function CategoryPage({ params }: CategoryPageProps) {
-	return <CategoryClientPage category={decodeURIComponent(params.category)} />;
+export default async function  CategoryPage({ params }: CategoryPageProps) {
+	const { category } = await params || {}
+	return <CategoryClientPage category={decodeURIComponent(category)} />;
 }
