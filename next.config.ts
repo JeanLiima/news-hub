0928d45c8cb4c8c7502d5f746829aa11315673ac
JeanLiima <jean.car.liima@gmail.com>
2025-07-25 19:32:54 -0300

    Fix build

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
-const baseConfig: NextConfig = {
-	reactStrictMode: true,
+const nextConfig: NextConfig = {
+	reactStrictMode: true
 };
 
-const nextConfig = withPWA({
-	...baseConfig,
-	pwa: {
-		dest: "public",
-		register: true,
-		skipWaiting: true,
-		disable: process.env.NODE_ENV === "development",
-	},
-});
-
-export default nextConfig;
+export default withPWA({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
+})(nextConfig);
